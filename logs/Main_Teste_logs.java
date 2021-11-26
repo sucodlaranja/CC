92c2cfe7534d0c4dcd6ef713a9e5df0ae3f8c9d9
LCMJ21 <jorge.lima.ne@gmail.com>
2021-11-26 16:16:10 +0000

    Alterei o nome do main de logs

---
+++ logs/Main_Teste_logs.java
@@ -0,0 +1,27 @@
+import javax.imageio.IIOException;
+import java.io.IOException;
+import java.util.concurrent.TimeUnit;
+
+public class Main_Teste_logs {
+    public static void main(String[] args) {
+        try {
+            Logs l1 = new Logs("C:\\Users\\jorge\\OneDrive\\Ambiente de Trabalho\\aa");
+            l1.printLogs();
+            Logs l2 = new Logs("C:\\Users\\jorge\\OneDrive\\Ambiente de Trabalho\\bb");
+
+
+            TimeUnit.MINUTES.sleep(0);
+
+            System.out.println("------------------------------------");
+            l2.updateFileLogs();
+            l2.printLogs();
+
+            System.out.println("------------------------------------");
+            l1.printTransfers(l1.compareLogs((l2.getLogs())));
+        }
+        catch (IOException | InterruptedException e) {
+            e.printStackTrace();
+        }
+
+    }
+}
