551fdaaf7ae10e2d9a4ff0bdbb561e999bc3c01b
LCMJ21 <jorge.lima.ne@gmail.com>
2021-12-03 19:58:38 +0000

    locks a funcionar no transfer handler

--- logs/TransferLogs.java
+++ logs/TransferLogs.java
@@ -1,17 +1,14 @@
 /**
  * Esta classe representa os logs de um ficheiro.
- * */
-public class TransferLogs {
-    private final String fileName;
-    private final boolean  senderOrReceiver; // true if i am sender
+ */
+public record TransferLogs(String fileName, boolean senderOrReceiver) {
 
-    public TransferLogs(String fileName, boolean senderOrReceiver){
-        this.fileName = fileName;
-        this.senderOrReceiver = senderOrReceiver;
+    public String getFileName() {
+        return fileName;
     }
 
-    public String getFileName(){ return fileName; }
-
-    public boolean isSenderOrReceiver() {return senderOrReceiver; }
+    public boolean isSenderOrReceiver() {
+        return senderOrReceiver;
+    }
 
 }
