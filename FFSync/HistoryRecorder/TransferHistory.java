fb4803b9139b4c573e4ff9da426714ab9137ba8d
LCMJ21 <jorge.lima.ne@gmail.com>
2021-12-15 20:37:10 +0000

    Adicionado as classes para imprimir em html v3

--- FFSync/HistoryRecorder/TransferHistory.java
+++ FFSync/HistoryRecorder/TransferHistory.java
@@ -1,35 +1,55 @@
 package HistoryRecorder;
 
+import Logs.TransferLogs;
+
+import java.io.*;
 import java.nio.file.attribute.FileTime;
-import java.util.HashMap;
-import java.util.HashSet;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class TransferHistory {
 
-    private Map<String,FileTransferHistory> files;
+    private final Map<String,FileTransferHistory> files;
 
     public TransferHistory(){
         files = new HashMap<>();
     }
 
-    public TransferHistory(String filepath){
-
+    public TransferHistory(String filepath) throws IOException, ClassNotFoundException {
+        ObjectInputStream is =
+                new ObjectInputStream(new FileInputStream(filepath));
+        TransferHistory transferHistory = (TransferHistory) is.readObject();
+        this.files = transferHistory.files;
     }
 
-    /*
+    public void updateLogs(Set<String> fileNames){
+        List<String> remove = new ArrayList<>();
+        for (Map.Entry<String, FileTransferHistory> file : files.entrySet())
+            if(!fileNames.remove(file.getKey())) remove.add(file.getKey());
 
-    public updateLogs(Set<String> fileNames){
-        //for (String file : fileNames) if (!files.containsKey(file)) files.put(file,new FileTransferHistory())
+        for(String file : remove) files.remove(file);
+
+        for (String file : fileNames)
+            files.put(file,new FileTransferHistory(null,-1,-1));
+    }
+
+    public void updateGuide(Queue<TransferLogs> guide){
+        for(TransferLogs fileTransfer:guide)
+            files.replace(fileTransfer.getFileName(),
+                    new FileTransferHistory(FileTime.fromMillis(0),10,10));
     }
 
 
-    public saveTransferHistory(String filepath){
-
+    public void saveTransferHistory(String filepath) throws IOException {
+        ObjectOutputStream os =
+                new ObjectOutputStream(new FileOutputStream(filepath));
+        os.writeObject(this);
+        os.flush();
+        os.close();
     }
 
-    public String toHTML(){}
-    */
+    public String toHTML(){
+        return "";
+    }
+
 
 }
