df7ee2c4538f17911fc7ee88c320d8b957d1ef92
LCMJ21 <jorge.lima.ne@gmail.com>
2021-12-15 19:53:08 +0000

    Adicionado as classes para imprimir em html

---
+++ FFSync/Logs/FileTransferHistory.java
@@ -0,0 +1,25 @@
+package Logs;
+
+import java.nio.file.attribute.FileTime;
+
+/**
+ * Represents entry of a transfer.
+ */
+public record FileTransferHistory(String fileName, FileTime lastUpdated, int timeOfTransfer, int bitsPSeg) {
+
+    public String getFileName() {
+        return fileName;
+    }
+
+    public FileTime lastUpdated() { return lastUpdated; }
+
+    public int timeOfTransfer() { return timeOfTransfer; }
+
+    public int bitsPSeg() { return bitsPSeg; }
+
+    public String toHTML(){
+        // TODO : JAO FAZER
+        return "";
+    }
+
+}
---
+++ FFSync/Logs/TransferHistory.java
@@ -0,0 +1,21 @@
+package Logs;
+
+import java.util.Map;
+import java.util.Set;
+
+public class TransferHistory {
+
+    private Set<FileTransferHistory> files;
+    /*
+    public TransferHistory(){}
+
+    public TransferHistory(String filepath){
+
+    }
+
+    public saveTransferHistory(String filepath){}
+
+    public String toHTML(){}
+    */
+
+}
